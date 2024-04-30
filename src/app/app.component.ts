3a94f56e596ef025778696bf4726f6f1b5a4f829
Niamh <niamh.linguist@gmail.com>
2024-04-30 15:47:08 +0300

    cleared app html file and added button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,12 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'intro';
+  title = 'Niamh';
+  myBtn = 'Click me';
+
+  // attr binding
+  isDisabled = false;
 }
